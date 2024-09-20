7fb2c3b9b59fa4c7c124d9479af5de8a3d1777d0
0Tung123 <harrybother356@gmail.com>
2024-09-20 11:31:43 +0700

    HomePage

---
+++ app/(tabs)/profile/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+export default function profile() {
+  return(
+    <View>
+        <Text>
+            trang profile đầy rực rỡ
+        </Text>
+    </View>
+  )
+}
--- constants/constants.ts
+++ constants/constants.ts
@@ -1,17 +1,21 @@
+import introImage1 from '@/assets/onboarding/intro_1.png';
+import introImage2 from '@/assets/onboarding/intro_2.png';
+import introImage3 from '@/assets/onboarding/intro_3.png';
+
 export const onboardingSwiperData: onboardingSwiperDataType[] = [
     {
         id: 1,
         title: "Start your learning journey with dose of fun!",
         description: "Our engaging learning videos will spark",
         sortDescription: "your curiosity & enjoyable.",
-        image: require("@/assets/onboarding/intro_1.png"),
+        image: introImage1,
     },
     {
         id: 2,
         title: "Discover your passion, elevate your expertise",
         description: "Our comprehensive our courses and expert",
         sortDescription: "instructors to guide every step.",
-        image: require("@/assets/onboarding/intro_2.png"),
+        image: introImage2,
     },
     {
         id: 3,
@@ -19,6 +23,6 @@ export const onboardingSwiperData: onboardingSwiperDataType[] = [
         description: "To earn an online certificate, individuals",
         sortDescription: "typically need to complete the required",
         sortDescription2: "coursework & assessments.",
-        image: require("@/assets/onboarding/intro_3.png"),
+        image: introImage3,
     },
 ];
