53e4de3793648c6f0cd477c31017e59d065e0dda
NguyenMinh993 <nguyenminh090903@gmail.com>
2024-11-06 05:43:41 +0700

    done real

--- screens/exams/examsResults.screen.tsx
+++ screens/exams/examsResults.screen.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/API/Exams/examAPI";
 import { ExamResults } from "@/constants/Exams/examResults";
 import { Exam, Question } from "@/constants/Exams/exam";
-import { useNavigation, useRouter } from "expo-router";
+import {useRouter } from "expo-router";
 
 
 export default function ExamResultScreen() {
@@ -22,7 +22,6 @@ export default function ExamResultScreen() {
   const [examData, setExamData] = useState<ExamResults | null>(null);
   const [examQuestions, setExamQuestions] = useState<Question[]>([]);
   const [hasAttempted, setHasAttempted] = useState<boolean>(false);
-  const navigation = useNavigation();
   useEffect(() => {
     const fetchExamData = async () => {
       const token = `Bearer ${await AsyncStorage.getItem("token")}`;
@@ -142,8 +141,7 @@ export default function ExamResultScreen() {
 
       <TouchableOpacity
         style={styles.backButton}
-        onPress={() =>
-          navigation.goBack()
+        onPress={() => router.push({ pathname: "/(routes)/content/content-list" })
         }
       >
         <Text style={styles.backButtonText}>Go Back to Course List</Text>
